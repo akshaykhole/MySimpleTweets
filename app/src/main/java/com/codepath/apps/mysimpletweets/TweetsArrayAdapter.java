6f753eb082fc4debefaf48136982da8b0d9e9f8e
Akshay Khole <akshay@reversehack.in>
2016-10-28 07:01:17 -0700

    * Basic adapter for list of tweets

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -1,10 +1,18 @@
 package com.codepath.apps.mysimpletweets;
 
 import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,5 +24,28 @@ import java.util.List;
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
     public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
         super(context, android.R.layout.simple_list_item_1, tweets);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Tweet t = getItem(position);
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+
+        }
+
+        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+
+        tvBody.setText(t.getBody());
+        tvUserName.setText(t.getUser().getName());
+        ivProfileImage.setImageResource(android.R.color.transparent); // Clear out old image for a recycled view
+
+        Picasso.with(getContext()).load(t.getUser().getProfileImageUrl()).into(ivProfileImage);
+
+        return convertView;
     }
 }
