6fd7031bbb0020c67e064db5ee6bf8ddeb1e95e5
Akshay Khole <akshay@reversehack.in>
2016-10-29 12:19:21 -0700

    Adds Nathans date parser helper

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ParseRelativeDate.java
@@ -0,0 +1,33 @@
+package com.codepath.apps.mysimpletweets;
+
+import android.text.format.DateUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created by akshay on 10/29/16.
+ */
+
+// Taken from https://gist.github.com/nesquena/f786232f5ef72f6e10a7
+public class ParseRelativeDate {
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+}
--- app/src/main/java/com/codepath/apps/mysimpletweets/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TwitterClient.java
@@ -53,7 +53,6 @@ public class TwitterClient extends OAuthBaseClient {
         RequestParams params = new RequestParams();
         params.put("count", 25);
         params.put("since_id", 1);
-
         getClient().get(apiUrl, params, handler);
     }
 
